b730b083a5e92f4c7841b700c98bf4f763697caa
Shaik Rasool <rasoolshaik1520@gmail.com>
2026-02-22 13:20:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+document.getElementById("surveyForm").addEventListener("submit",function(e){
+e.preventDefault();
+alert("Thank you for participating in the survey!");
+this.reset();
+});
+</script>
